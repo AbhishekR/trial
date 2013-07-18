537c65eee8373c4453af7a979283db6e9a7aa397
stack <stack@stack-virtual-machine.(none)>
2013-07-18 16:56:26 -0400

    updated

--- check.sh
+++ check.sh
@@ -5,18 +5,19 @@
 #sudo adduser `id -un` libvirtd
 #sudo apt-get install virt-manager
 #clone devstack 
-~/git clone git://github.com/openstack-dev/devstack.git
+
+git clone git://github.com/openstack-dev/devstack.git
 #install openstack
-~/devstack ./stack.sh
+~/devstack/stack.sh
 #copy all9s recovery module to openstack
-cp -r recovery /opt/stack/horizon/openstack_dashboard/dashboards/project
+cp -r ~/trial/recovery /opt/stack/horizon/openstack_dashboard/dashboards/project
 #replacing the dashboard
 rm /opt/stack/horizon/openstack_dashboard/dashboards/project/dashboard.py
 rm /opt/stack/horizon/openstack_dashboard/static/dashboard/logo.png
 rm /opt/stack/horizon/openstack_dashboard/static/dashboard/logo-splash.pn
-cp logo.png /opt/stack/horizon/openstack_dashboard/static/dashboard//img/logo.png
-cp logo.png /opt/stack/horizon/openstack_dashboard/static/dashboard/img/logo-splash.png
-cp dashboard.py /opt/stack/horizon/openstack_dashboard/dashboards/project
+cp ~/trial/logo.png /opt/stack/horizon/openstack_dashboard/static/dashboard//img/logo.png
+cp ~/trial/logo.png /opt/stack/horizon/openstack_dashboard/static/dashboard/img/logo-splash.png
+cp ~/dashboard.py /opt/stack/horizon/openstack_dashboard/dashboards/project
 # restart the apache server
 sudo service apache2 restart
 
